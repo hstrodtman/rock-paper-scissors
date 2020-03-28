7cc84cda477e314aed8c3f7748daa020fda67bbc
hstrodtman <heatherstrodtman@gmail.com>
2020-03-28 15:51:42 -0500

    refactored code, added reset button with functionality, and prompt validation.

--- rps.js
+++ rps.js
@@ -1,44 +1,54 @@
 let outcome;
 
-//plays one round of rock, paper, scissors
-
 window.addEventListener("load", function() {
     let compSelect = document.getElementById("computer"); 
-    let playerChoiceButton = document.getElementById("choice");  
+    let playerChoiceButton = document.getElementById("choice"); 
+    let resetButton = document.getElementById('reset');  
+    let outcomeTextLocation = document.getElementById("outcome");
 
-        let random = Math.floor(Math.random() * 3); 
-        if (random===0) {
-            outcome ='rock'; 
-            } else if (random===1) {
-            outcome ='paper'; 
-            } else {
-            outcome='scissors';
-        }
-        // return outcome; 
 
 playerChoiceButton.onclick = function(){
-
+ 
     let playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase(); 
-
-    let outcomeTextLocation = document.getElementById("outcome"); 
+    let random = Math.floor(Math.random() * 3); 
+         
+    if (random===0) {
+        outcome ='rock'; 
+        } else if (random===1) {
+        outcome ='paper'; 
+        } else {
+        outcome='scissors';
+        }
+ 
+    while (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors'){
+        playerSelection = prompt('Invalid Entry. You must enter rock, paper, or scissors').toLowerCase(); 
+    }
   
     playerChoiceButton.innerHTML = playerSelection; 
     
     compSelect.innerHTML= outcome;
 
     if (playerSelection==="rock" && outcome==='scissors' || playerSelection===
-    'paper' && outcome==='rock' || playerSelection==='scissors' && outcome==='paper') { 
+        'paper' && outcome==='rock' || playerSelection==='scissors' && outcome==='paper') { 
         outcomeMessage = 'Yay, You Won!'; 
-    }
-    else if (playerSelection===outcome) {
+        }
+     else if (playerSelection===outcome) {
        outcomeMessage = 'Tie, Try Again!'; 
     } else {
         outcomeMessage = "Boo, You Lost!"; 
     }
-
     outcomeTextLocation.innerHTML = outcomeMessage; 
-}
 
+    resetButton.style.display = "inline"; 
+
+    }  
+
+    
+    resetButton.onclick = function() {
+        compSelect.innerHTML = "Hidden"; 
+        playerChoiceButton.innerHTML= "Make a Selection"; 
+        outcomeTextLocation.innerHTML = "Who Will Win?"
+    }
 
  }); 
 
